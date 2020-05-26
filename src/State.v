78d26d476f35748a8027b32b44c67b7c5c26f923
Kartik Singhal <ks@cs.uchicago.edu>
2020-05-26 08:59:22 -0500

    Partial and broken state modeling similar to QIO

--- src/State.v
+++ src/State.v
@@ -1,17 +1,43 @@
 Require Import QHTT.Axioms.
 Require Import Arith.
+Require Import Coq.FSets.FMapList.
+Require Import Coq.Structures.OrderedTypeEx.
+Require Import VQC.Complex.
+
+(* Following QIO, we define quantum state as a sum over all base states and their
+   corresponding complex amplitudes *)
 
 (* location or label to refer to a qubit *)
 Definition loc := nat.
+
+(* bases are stored as a map from qubits to booleans *)
+Module Import LocMap := FMapList.Make(Nat_as_OT).
+
+(* primitive operations on bases *)
+Definition initial := LocMap.empty.
+Definition update m (q : loc) (b : bool) := LocMap.add q b m.
+Definition lookup m (q : loc) : option bool := LocMap.find q m.
+
+(* quantum state is a list of pairs where each pair stores a basis state
+   and the amplitude associated with that basis state *)
+Class State (A : Type) (B : Type) :=
+  { sempty : A -> C -> B ;
+    scombine : (A -> C -> B) -> (A -> C -> B) -> A -> C -> B ;
+    sscale : C -> (A -> C -> B) -> A -> C -> B ;
+    slookup : B -> (A -> C -> B) -> A
+  }.
+
+(* BROKEN CODE FOLLOWS *)
+Instance qstate (B : Type) : State (list (C, B)) :=
+  { sempty := nil;
+  }.
+Parameter state (A : Type) : list pair C A.
+
 Definition val := dynamic%type.
 
 (* state is a partial function from locations to dynamic
   (that stores both type and value, ie, abstracted type) *)
 Definition state := loc -> option val.
-
-(** * Useful state assertions *)
-
-Definition empty : state := fun _ => None.
 
 Definition singleton (l : loc) (v : val) : state :=
   fun l' => if eq_nat_dec l l' then Some v else None.
